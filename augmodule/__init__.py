400feb8884a9ad6d67b22c1e3b6cbc660d371f50
Mohammad <mohammad.akhavan75@gmail.com>
2025-08-12 17:00:08 +0330

    adding gaussian noise

--- augmodule/__init__.py
+++ augmodule/__init__.py
@@ -1,10 +1,13 @@
 from .modules.glass_blur import GlassBlur
 from .modules.flip import Flip, RandomFlip
 from .modules.rotation import Rotate, RandomRotate
+from .modules.noise import RandomGaussianNoise
+
 
 __all__ = [
     "GlassBlur",
     "Flip", "RandomFlip",
     "Rotate", "RandomRotate",
+    "RandomGaussianNoise",
 ]
 __version__ = "0.1.0"
